a386e0c9a44e240cbcc9bb0bd0ed4d5e971108cd
David Hovemeyer <david.hovemeyer@gmail.com>
2018-06-14 08:54:27 -0400

    initial commit

---
+++ i82c55a_test.ino
@@ -0,0 +1,28 @@
+// Test 82C55A using Arduino
+
+// Connections:
+// 82C55A            Arduino
+// ----------------------------------------
+// D0                D0(0)
+// D1                D1(1)
+// D2                D2(2)
+// D3                D3(3)
+// D4                D4(4)
+// D5                D5(5)
+// D6                D6(6)
+// D7                D7(7)
+// A0                C0(14)
+// A1                C1(15)
+// -CS               C2(16)
+// -RD               C3(17)
+// -WR               C4(18)
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
